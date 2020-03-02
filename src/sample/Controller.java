66040e163ed5fa292f25102a5edee6e56780f056
Kacper Jeczmien <kacperj9713@gmail.com>
2020-03-02 06:18:40 +0100

    #2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.math.BigDecimal;
+import java.text.*;
+
 public class Controller {
+
+    private BigDecimal left;
+    private String selectedOperator;weqweqwe
+    private boolean numberInputting;
+
+    @FXML
+    private TextField textField;
+    public Controller() {
+        this.left = BigDecimal.ZERO;
+        this.selectedOperator = " ";
+        this.numberInputting = false;
+
+    }
+
+
+
+    @FXML
+    public void operateButton(ActionEvent evt) {
+        Button button = (Button)evt.getSource();
+        String buttonText = button.getText();
+
+
+    }
+
 }
