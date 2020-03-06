86d4b566f6949904ef5865d7c3ec17a2b4d21b19
Kacper Jeczmien <kacperj9713@gmail.com>
2020-03-06 14:20:37 +0100

    Operate Button'

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,16 +5,23 @@ import javafx.fxml.FXML;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.math.BigDecimal;
-import java.text.*;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class Controller {
 
     private BigDecimal left;
-    private String selectedOperator;weqweqwe
+    private String selectedOperator;
+    weqweqwe
     private boolean numberInputting;
 
     @FXML
     private TextField textField;
+
     public Controller() {
         this.left = BigDecimal.ZERO;
         this.selectedOperator = " ";
@@ -23,12 +30,40 @@ public class Controller {
     }
 
 
-
     @FXML
     public void operateButton(ActionEvent evt) {
-        Button button = (Button)evt.getSource();
+        Button button = (Button) evt.getSource();
         String buttonText = button.getText();
 
+        if (buttonText.equals("C")) {
+            if (buttonText.equals("C")) {
+                left = BigDecimal.ZERO;
+            }
+
+            selectedOperator = " ";
+            numberInputting = true;
+            textField.clear();
+            return;
+        }
+
+        if (buttonText.matches("[0-9\\,1]")) {
+            if (!numberInputting) {
+                numberInputting = true;
+                textField.clear();
+            }
+
+            textField.appendText(buttonText);
+            return;
+        }
+
+        if (buttonText.matches("+-*/")) {
+            left = new BigDecimal(textField.getText());
+            selectedOperator = buttonText;
+            numberInputting = false;
+            return;
+        }
+
+        
 
     }
 
