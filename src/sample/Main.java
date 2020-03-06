f8341da61b3d9286caf46d543cddc8e54ff217a2
Kacper Jeczmien <kacperj9713@gmail.com>
2020-03-06 15:23:39 +0100

    Buttons added to HBox'es and childrens

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,13 +9,17 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Calculator");
-        primaryStage.show();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Calculator");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
